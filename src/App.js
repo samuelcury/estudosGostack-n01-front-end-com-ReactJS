fa57bf5ea0026954653b2e23ccf03f673bdd8dcb
Samuel Cury <cury.samuel90@gmail.com>
2020-12-11 22:14:36 -0300

    imagem importada no app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import './App.css';
 
+import backgroundImg from './assets/background.jpg'
+
 import Header from './components/Header';
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
     return (
         <>
             <Header title="Projects"/>
+
+            <img src={ backgroundImg } />
 
             <ul>
                 {projects.map(project => <li key={project}>{project}</li>)}
